8a5904b5de7f8aaed6c97445b464ebf12d3d5765
Felipe Guedes <felipeguedesk@outlook.com>
2021-02-06 20:28:42 -0600

    Week 1 ICD material
    ICD - introdução a ciência de dados

--- modulo_1/Introducao-Data-Science/Week_1/scratch.py
+++ modulo_1/Introducao-Data-Science/Week_1/scratch.py
@@ -1,55 +1,75 @@
 from statistics import mode
 import numpy as np 
 import matplotlib.pyplot as plt #biblioteca gráfica para mostrar os gráficos
-X = [0,1,1,2,2,2,3,3]
-values = np.sort(np.unique(X)) 
-# vamos calcular a frequência de cada valor presente na lista
-freq = np.zeros(len(values)) # armazena as frequencias
-ind = 0 # indice do vetor de frequências
-for i in values: # para os valores diferentes
-    counter = 0 # conta as ocorrências
-    for j in range(0,len(X)):
-        if(X[j] == i):
-            counter = counter + 1
-    freq[ind] = counter
-    ind = ind + 1
+import pandas as pd # biblioteca pandas
+from scipy.stats import entropy
+from scipy.stats import iqr
+import math
+from scipy.stats import pearsonr, spearmanr
 
-print('X:',X)
-print('Valores possíveis:', values)
-print('Frequencia dos valores:', freq)
+# # Exercicios de fixação:
+# # 1)
+# result_map = {}
+# for p in np.arange(0, 1.01, 0.01):
+#     result_map[p] = entropy([p, 1-p], base=2)
 
-y_pos = np.arange(len(values))
-plt.xticks(y_pos,values)
-plt.bar(y_pos, freq)
-plt.xlabel("Valores",fontsize = 20) 
-plt.ylabel("Frequência", fontsize = 20) 
-plt.show()
+# plt.figure(num=1)
+# plt.plot(result_map.keys(), result_map.values()) 
+# plt.xlabel("Entropy",fontsize = 20) 
+# plt.ylabel("Probability of heads", fontsize = 20)
+# plt.show()
+# var = np.var(p)
 
+# # 2)
+# poisson_results = {}
+# for l in np.arange(0, 0.4, 0.1):
+#     distribution = np.random.poisson(l, 500)
+#     poisson_results[l] = [np.mean(distribution), np.var(distribution)]
+# means = [meanVar[0] for meanVar in poisson_results.values() if isinstance(meanVar, list) and len(meanVar) > 1]
+# variances = [meanVar[1] for meanVar in poisson_results.values() if isinstance(meanVar, list) and len(meanVar) > 1]
+# plt.figure(num=2)
+# plt.plot(poisson_results.keys(), means, color='blue', label='Mean') 
+# plt.plot(poisson_results.keys(), variances, color='red', label='Var') 
+# plt.xlabel("Poisson lambda",fontsize = 20) 
+# plt.ylabel("Mean/Var", fontsize = 20)
+# plt.legend()
+# plt.show()
 
+# Weekly Evaluation
+# 1 
+# Z = [0,0,2,3,1,2,4,5,6,8,0,0,7,0,1,2,1,1, 5,3,1,0,7,3,2,3,4,5,6,7,8,9,1,1,2,2,3]
+# print("A moda é: {}".format(mode(Z)))
 
-Y = ["red", "blue", "blue", "red", "green", "red", "red"]
-print('A moda de Y:', mode(Y))
-values = np.sort(np.unique(Y)) #usamos a funções sort e unique do Numpy para encontrar os valores possíveis.
-# vamos calcular a frequência de cada valor presente na lista
-freq = np.zeros(len(values)) # armazena as frequencias
-ind = 0 # indice do vetor de frequências
-for i in values: # para os valores diferentes
-    counter = 0 # conta as ocorrências
-    for j in range(0,len(Y)):
-        if(Y[j] == i):
-            counter = counter + 1
-    freq[ind] = counter
-    ind = ind + 1
+# # 2
+# Z = [0,0,2,3,1,2,4,5,6,8,0,0,7,0,1,2,1,1, 5,3,1,0,7,3,2,3,4,5,6,7,8,9,1,1,2,2,3]
 
-print('Y:',Y)
-print('Valores possíveis:', values)
-print('Frequencia dos valores:', freq)
+# print("The mean is {} and the median is {}".format(np.mean(Z), np.median(Z)))
 
-y_pos = np.arange(len(values))
-plt.xticks(y_pos, values)
-plt.bar(y_pos, freq)
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
-plt.xlabel("Valores",fontsize = 20) 
-plt.ylabel("Frequência", fontsize = 20) 
-plt.show()
+# # 3
+# Z = [0,0,2,3,1,2,4,5,6,8,7,5,3,1,0,7,3,2,3,4,5,6,7,8,9,1,1,2,2,3]
+# def variancia(X):
+#     m = np.mean(X)
+#     N = len(X)
+#     s = 0
+#     for i in np.arange(0, len(X)):
+#         s = s + (X[i]-m)**2
+#     s = s/(N-1)
+#     return s
+
+# print("Var={},StdDev={},IQR={}".format(variancia(Z), math.sqrt(variancia(Z)), iqr(Z)))
+
+# # 4
+
+# x = np.linspace(-1.5, 1.5, num=100)
+# tan = [math.tan(value) for value in x]
+# corr, p_value = pearsonr(x, tan)
+# corrs, p_values = spearmanr(x, tan)
+# corr = int(corr*100)/100
+# corrs = int(corrs*100)/100
+# print("Pearson={}, Spearman={}".format(corr, corrs))
+
+# 5
+P = [0.15,0.25,0.2]
+Q = [0.3, 0.6, 0.1]
+print('KL(P,Q) = ', entropy(P,Q, base = np.exp(1)))
+print('KL(Q,P) = ', entropy(Q,P, base = np.exp(1)))
